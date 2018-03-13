a009f3f9144c0f43eb7f9f97f6de68120a122b61
Kevin_Dev <kevin160718@163.com>
2018-03-13 12:36:10 +0800

    fix a little

---
+++ src/main/java/com/zjitc/test/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.zjitc.test.mapper;
+
+/**
+ * Create with IntelliJ IDEA
+ * User : kevin
+ * Dare : 2018/3/13
+ * Time : 12:26
+ * To change this template use File | Setting | File Template.
+ * Description :
+ */
+public interface UserMapper {
+
+}
